4513f95a2980ffc4c5ae5cc367477543bdd6a6e4
tayoky <tayoky4848@gmail.com>
2026-07-19 13:59:02 +0200

    init : load default from config.mk

--- make/tmake-init.mk
+++ make/tmake-init.mk
@@ -1,6 +1,11 @@
 # makefile include with default values
 MAKEFLAGS += --no-builtin-rules
+
+TOP ?= $(CURDIR)
+-include $(TOP)/config.mk
+
 CC ?= cc
 LD ?= ld
 AS ?= as
+PREFIX ?= /usr/local
 BUILDDIR ?= $(CURDIR)/build
